b1ef4c35d5fde9a9061bb94feb9813c73539bb79
SekikawaHayato <hayato.901i.softbank@gmail.com>
2020-10-01 15:21:24 +0900

    storyboard作成

---
+++ OriginalApp/WriteDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateDataViewController.swift
+//  OriginalApp
+//
+//  Created by 関川隼人 on 2020/10/01.
+//  Copyright © 2020 関川隼人. All rights reserved.
+//
+
+import UIKit
+
+class WriteDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
